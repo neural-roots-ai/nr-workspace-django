d76b5f3d77e2e020da0ad37694f401df05c4232b
PRATEEK SAHU <43596921+prateeksahu147@users.noreply.github.com>
2025-05-21 22:21:50 +0530

    add allowed host

--- src/workspace/settings.py
+++ src/workspace/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['workspace.neuralroots.in', '147.93.102.99']
 
 
 # Application definition
